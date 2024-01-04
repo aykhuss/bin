2512c2e3b3e3e10d37775cd4c7aebb4760df88fe
Alexander Huss <alexander.huss@cern.ch>
2024-01-04 14:44:17 +0100

    mov2gif.sh: make `-o` optional (defaults to <IN>.gif); add optional `--fps` option.

--- mov2gif.sh
+++ mov2gif.sh
@@ -2,25 +2,29 @@
 
 usage() {
   echo
-  echo "usage:  mov2gif.sh [-h] -i <in>.mov -o <out>.gif"
+  echo "usage:  mov2gif.sh [-h] -i <IN>.mov [-o <OUT>.gif] [--fps <INT>]"
   echo
   echo "description:  convert 'mov' files into 'gif's."
   echo "options:"
   echo "   -h         print help"
-  echo "   -i         input  .mov file"
-  echo "   -o         output .gif file"
+  echo "   -i         input  <IN>.mov  file"
+  echo "   -o         output <OUT>.gif file (default: '<IN>.gif')"
+  echo "   --fps      frames per second in conversion"
   echo
 }
 
+#> check needed commands are available on the system
 if ! command -v ffmpeg &> /dev/null; then
   echo "requirement: 'ffmpeg' could not be found"
   exit 1
 fi
-
 if ! command -v gifsicle &> /dev/null; then
   echo "requirement: 'gifsicle' could not be found"
   exit 1
 fi
+
+#> defaults
+fps=10
 
 while [[ -n $1 ]]; do
   case $1 in
@@ -38,6 +42,11 @@ while [[ -n $1 ]]; do
       out_gif="$1"
       shift
       ;;
+    --fps)
+      shift
+      fps="$1"
+      shift
+      ;;
     *)
       usage
       echo "unknown option: '$1'"
@@ -46,16 +55,21 @@ while [[ -n $1 ]]; do
   esac
 done
 
+#> checks
 if ! [[ -n ${in_mov} ]]; then
   usage
   echo "missing '-i <in>.mov' option"
   exit 1
 fi
-
-if ! [[ -n ${out_gif} ]]; then
+if ! [[ ${fps} =~ ^[0-9]+$ ]]; then
   usage
-  echo "missing '-o <out>.gif' option"
+  echo "fps must be an integer: ${fps}"
   exit 1
 fi
 
-ffmpeg -i ${in_mov} -pix_fmt rgb8 -r 10 ${out_gif} && gifsicle -O3 ${out_gif} -o ${out_gif}
+#> no outputgiven:  derive from input file
+if ! [[ -n ${out_gif} ]]; then
+  out_gif=${in_mov%.*}.gif
+fi
+
+ffmpeg -i ${in_mov} -pix_fmt rgb8 -r ${fps} ${out_gif} && gifsicle -O3 ${out_gif} -o ${out_gif}
